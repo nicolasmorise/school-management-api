725619e039542c046d2c136cfc0862fc1888cf39
delacruzsamb@gmail.com <121100632+samueldelacruz@users.noreply.github.com>
2025-12-04 11:12:32 +0800

    Added staff routes and controller/model

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,7 @@ const router = require('express').Router();
 // Student Route - samueldelacruz123
 router.use('/students', require('./studentsRoute'));
 
+// Staff Route - samueldelacruz123
+router.use('/staff', require('./staffRoute'));
+
 module.exports = router;
